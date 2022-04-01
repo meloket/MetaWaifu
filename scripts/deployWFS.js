885adf9b2ac67a259ce345af3d861e305312736e
unknown <cloverdev88@gmail.com>
2022-04-01 09:28:28 -0400

    first tow sale contract done

--- scripts/deployWFS.js
+++ scripts/deployWFS.js
@@ -1,10 +1,10 @@
 async function main() {
     const Waifus = await ethers.getContractFactory("Waifus")
-    const SDT_Contract_Addr = "";
+    const SDT_Contract_Addr = "0x5c117723c91c24B8A3411d4BE19B9e9c603b5051";
   
     // Start deployment, returning a promise that resolves to a contract object
-    const myContract = await Waifus.deploy()
-    await myContract.deployed(SDT_Contract_Addr);
+    const myContract = await Waifus.deploy(SDT_Contract_Addr)
+    await myContract.deployed();
     console.log("Contract deployed to address:", myContract.address)
   }
   
